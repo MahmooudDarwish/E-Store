6646a5b8548eea058f232b433a035e3b0dd0a3f0
pc <mahmoudsaaddarwish1@gmail.com>
2024-10-04 00:50:17 +0300

    get products from custom collection rather than getting it from the whole products

--- app/src/main/java/com/example/e_store/utils/constants/APIKeys.kt
+++ app/src/main/java/com/example/e_store/utils/constants/APIKeys.kt
@@ -6,7 +6,7 @@ object APIKeys {
     const val SHOPIFY_API_KEY = "6f72d418fcb2c0867c10ff051079310e"
     const val ADMIN_API_ACCESS_TOKEN = "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"
     const val STORE_URL = "android-sv24-r3team3.myshopify.com"
-    const val SHOPIFY_BASE_URL = "https://$SHOPIFY_API_KEY:$ADMIN_API_ACCESS_TOKEN@$STORE_URL/admin/api/2024-07/"              //{resource}.json"
+    const val SHOPIFY_BASE_URL = "https://$SHOPIFY_API_KEY:$ADMIN_API_ACCESS_TOKEN@$STORE_URL/admin/api/2024-07/"
     const val SHOPIFY_API_SECRET_KEY = "2d54b067f29fe607ccecd2c91294acb3"
     const val STOREFRONT_API_ACCESS_TOKEN = "7d0ecff0b6c5a01e2350a2c44428ae0f"
 
@@ -19,6 +19,9 @@ object APIKeys {
     const val PRODUCTS_ENDPOINT = "products.json"
 
 
+    //Values
+
+    const val HOME = "Home"
 }
 
 
--- app/src/main/java/com/example/e_store/utils/data_layer/remote/EStoreRemoteDataSource.kt
+++ app/src/main/java/com/example/e_store/utils/data_layer/remote/EStoreRemoteDataSource.kt
@@ -1,11 +1,14 @@
 package com.example.e_store.utils.data_layer.remote
 
 import com.example.e_store.utils.shared_models.Brand
+import com.example.e_store.utils.shared_models.CustomCollection
 import com.example.e_store.utils.shared_models.Product
 import kotlinx.coroutines.flow.Flow
 
 interface EStoreRemoteDataSource {
      suspend fun fetchBrands(): Flow<List<Brand>>
      suspend fun fetchForUProducts(): Flow<List<Product>>
+     suspend fun fetchCustomCollections(): Flow<List<CustomCollection>>
+
 
 }
