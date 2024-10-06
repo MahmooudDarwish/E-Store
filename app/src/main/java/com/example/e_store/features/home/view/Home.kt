86148ae3c2159614a3bed8ab05a545c58143a151
pc <mahmoudsaaddarwish1@gmail.com>
2024-10-07 00:26:52 +0300

    Add refresh feature to home screen

--- app/src/main/java/com/example/e_store/features/home/view/Home.kt
+++ app/src/main/java/com/example/e_store/features/home/view/Home.kt
@@ -3,15 +3,21 @@ package com.example.e_store.features.home.view
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -33,101 +39,100 @@ import com.example.e_store.utils.shared_components.Gap
 import com.example.e_store.utils.shared_models.DataState
 import androidx.compose.ui.res.stringResource
 
-
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
     val context = LocalContext.current
 
+    val brandsUiState by viewModel.brands.collectAsStateWithLifecycle()
+    val forUProductsUiState by viewModel.forUProducts.collectAsStateWithLifecycle()
+    val discountCodesUiState by viewModel.discountCodes.collectAsStateWithLifecycle()
+    val isRefreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
 
-        val brandsUiState by viewModel.brands.collectAsStateWithLifecycle()
-        val forUProductsUiState by viewModel.forUProducts.collectAsStateWithLifecycle()
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshing,
+        onRefresh = { viewModel.refreshAllData() }
+    )
 
-        val discountCodesUiState by viewModel.discountCodes.collectAsStateWithLifecycle()
-
-
-        val sliderImages = remember {
-            mutableStateListOf<SliderItem>().apply {
-                addAll(
-                    listOf(
-                        SliderItem(R.drawable.addsgifone, "New Collection of Shirts"),
-                        SliderItem(R.drawable.adsgiftwo, "New Collection of Shoes"),
-                        SliderItem(R.drawable.adsgifthree, "New Collection of Hoodies"),
-                        SliderItem(R.drawable.adsgiffour, "New Collection of Shirts"),
-                        SliderItem(R.drawable.adsgiffive, "New Collection of Hoodies"),
-                        SliderItem(R.drawable.adsgifsix, "New Collection of Shoes")
-                    )
+    val sliderImages = remember {
+        mutableStateListOf<SliderItem>().apply {
+            addAll(
+                listOf(
+                    SliderItem(R.drawable.addsgifone, "New Collection of Shirts"),
+                    SliderItem(R.drawable.adsgiftwo, "New Collection of Shoes"),
+                    SliderItem(R.drawable.adsgifthree, "New Collection of Hoodies"),
+                    SliderItem(R.drawable.adsgiffour, "New Collection of Shirts"),
+                    SliderItem(R.drawable.adsgiffive, "New Collection of Hoodies"),
+                    SliderItem(R.drawable.adsgifsix, "New Collection of Shoes")
                 )
-            }
+            )
+        }
+    }
+
+    when (discountCodesUiState) {
+        DataState.Loading -> {
+            EShopLoadingIndicator()
         }
 
-
-
-        when (discountCodesUiState) {
-            DataState.Loading -> {
-                EShopLoadingIndicator()
-            }
-
-            is DataState.Success -> {
-                (discountCodesUiState as DataState.Success).data?.let { data ->
-                    val codesToProcess = data.flatMap { it.discount_codes }.take(5)
-                    val newSliderItems = codesToProcess.map { discountCode ->
-                        val codeLower = discountCode.code.lowercase()
-                        val imageId = when (codeLower) {
-                            "fivepercentoff" -> R.drawable.fivepercentoff
-                            "tenpercentoff" -> R.drawable.tenpercentoff
-                            "fifteenpercentoff" -> R.drawable.fifteenpercentoff
-                            "twentypercentoff" -> R.drawable.twentypercentoff
-                            "twentyfivepercentoff" -> R.drawable.twentyfivepercentoff
-                            "thirtypercentoff" -> R.drawable.thirtypercentoff
-                            "freeship" -> R.drawable.freeship
-                            else -> 0
-                        }
-                        SliderItem(imageId, discountCode.code)
+        is DataState.Success -> {
+            (discountCodesUiState as DataState.Success).data?.let { data ->
+                val codesToProcess = data.flatMap { it.discount_codes }.take(5)
+                val newSliderItems = codesToProcess.map { discountCode ->
+                    val codeLower = discountCode.code.lowercase()
+                    val imageId = when (codeLower) {
+                        "fivepercentoff" -> R.drawable.fivepercentoff
+                        "tenpercentoff" -> R.drawable.tenpercentoff
+                        "fifteenpercentoff" -> R.drawable.fifteenpercentoff
+                        "twentypercentoff" -> R.drawable.twentypercentoff
+                        "twentyfivepercentoff" -> R.drawable.twentyfivepercentoff
+                        "thirtypercentoff" -> R.drawable.thirtypercentoff
+                        "freeship" -> R.drawable.freeship
+                        else -> 0
                     }
-                    Log.d("HomeScreen", "newSliderItems: $newSliderItems")
-                    updateSliderImages(sliderImages, newSliderItems)
+                    SliderItem(imageId, discountCode.code)
                 }
-
+                Log.d("HomeScreen", "newSliderItems: $newSliderItems")
+                updateSliderImages(sliderImages, newSliderItems)
             }
 
-            is DataState.Error -> {
-                val errorMsg = (discountCodesUiState as DataState.Error).message
-                Log.e("HomeScreen", "Error fetching discount codes: $errorMsg")
-                LaunchedEffect(errorMsg) {
-                    Toast.makeText(context, errorMsg, Toast.LENGTH_SHORT).show()
-                }
+        }
+
+        is DataState.Error -> {
+            val errorMsg = (discountCodesUiState as DataState.Error).message
+            Log.e("HomeScreen", "Error fetching discount codes: $errorMsg")
+            LaunchedEffect(errorMsg) {
+                Toast.makeText(context, errorMsg, Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-        LaunchedEffect(Unit) {
-            viewModel.getBrands()
-            viewModel.getForUProducts()
-            viewModel.fetchDiscountCodes()
-        }
+    LaunchedEffect(Unit) {
+        viewModel.refreshAllData()
+    }
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState)
+            .background(color = Color.White)
+    ) {
         LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.White)
+            modifier = Modifier.fillMaxSize()
         ) {
-
             item {
                 SearchWithFavoriteSection(
                     onSearchClick = {
-                        ///TODO: Navigation to search screen  @kk98989898
                         navController.navigate(Screen.Search_From_Home.route)
                     },
                     onFavoriteClick = {
-                        ///TODO: Navigation to Favorite screen @kk98989898
-                    },
+                        // Navigate to favorite screen
+                        ///TODO: Navigate to favorite products screen @kk989898
+                    }
                 )
             }
 
             item {
-                ///TODO: Integrate your slider Here @mohamed-abdelrehim142000
                 AdsSlider(initialImages = sliderImages) { clickedItem ->
-
-                    // Handle click on the slider item
                     Toast.makeText(
                         context,
                         "Coupons: ${clickedItem.title} Copied!",
@@ -135,8 +140,8 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                     ).show()
                 }
             }
-            item { Gap(height = 16) }
 
+            item { Gap(height = 16) }
 
             item {
                 Text(
@@ -146,13 +151,9 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                 )
             }
 
-
-
             item {
                 when (brandsUiState) {
-                    DataState.Loading -> {
-                        EShopLoadingIndicator()
-                    }
+                    DataState.Loading -> EShopLoadingIndicator()
 
                     is DataState.Success -> {
                         val brands = (brandsUiState as DataState.Success).data
@@ -168,8 +169,6 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                 }
             }
 
-
-
             item {
                 Text(
                     text = stringResource(id = R.string.for_you),
@@ -178,22 +177,17 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                 )
             }
 
-            item {
-                Gap(height = 16)
-            }
+            item { Gap(height = 16) }
 
             item {
                 when (forUProductsUiState) {
-                    DataState.Loading -> {
-                        EShopLoadingIndicator()
-                    }
+                    DataState.Loading -> EShopLoadingIndicator()
 
                     is DataState.Success -> {
                         val forUProducts = (forUProductsUiState as DataState.Success).data
                         ForUSection(
                             navController = navController,
-                            products = forUProducts,
-                            ///TODO: Navigation to product details screen  @MahmoudDarwish @kk98989898
+                            products = forUProducts
                         )
                     }
 
@@ -206,6 +200,12 @@ fun HomeScreen(viewModel: HomeViewModel, navController: NavHostController) {
                 }
             }
         }
+
+
+        PullRefreshIndicator(
+            refreshing = isRefreshing,
+            state = pullRefreshState,
+            Modifier.align(Alignment.TopCenter)
+        )
     }
-
-
+}
